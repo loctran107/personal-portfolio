c0dbc9b21ea9a7f6a89be611b6bda52d38546bc3
Donovan Tran <dontr710@gmail.com>
2025-02-26 12:11:51 -0800

    Added social media link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -50,9 +50,9 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
+                <a href="https://www.linkedin.com/in/loctran710/"><img src={navIcon1} alt="" /></a>
+                <a href="https://www.facebook.com/profile.php?id=100007684203635"><img src={navIcon2} alt="" /></a>
+                <a href="https://www.instagram.com/donovantran__/"><img src={navIcon3} alt="" /></a>
               </div>
               <HashLink to='#connect'>
                 <button className="vvd"><span>Let’s Connect</span></button>

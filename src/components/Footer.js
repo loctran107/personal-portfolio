768c0c9381aba256ff6fa581339f500d9bd1ae5f
Donovan Tran <dontr710@gmail.com>
2025-03-02 14:16:55 -0800

    Fixed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { MailchimpForm } from "./MailchimpForm";
-import logo from "../assets/img/logo.svg";
+import DTLogo from '../assets/img/DT_Logo.svg';
 import linkedinIcon from "../assets/img/linkedin-icon.svg";
 import githubIcon from "../assets/img/github_dark.png";
 import instaIcon from "../assets/img/insta-icon.svg";
@@ -12,7 +12,7 @@ export const Footer = () => {
         <Row className="align-items-center">
           <MailchimpForm />
           <Col size={12} sm={6}>
-            <img src={logo} alt="Logo" />
+            <img src={DTLogo} alt="Logo" />
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">

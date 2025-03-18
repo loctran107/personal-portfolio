5bb45b1ce8b9c5118b6f4561612e185260ee9518
Donovan Tran <dontr710@gmail.com>
2025-03-17 22:43:53 -0700

    Added Machine Learning SVG

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,6 +4,7 @@ import "react-multi-carousel/lib/styles.css";
 import meter1 from "../assets/img/meter1.svg";
 import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
+import machine_learning from "../assets/img/machine-learning.svg";
 import colorSharp from "../assets/img/color-sharp.png";
 
 
@@ -45,7 +46,7 @@ export const Skills = () => {
                             </p>
                             <Carousel responsive={responsive} infinite={true} className="skill-slider">
                                 <div className="item">
-                                    <img src={meter1} alt="Image" />
+                                    <img src={machine_learning} alt="Image" />
                                     <h5>Machine Learning</h5>
                                 </div>
                                 <div className="item">

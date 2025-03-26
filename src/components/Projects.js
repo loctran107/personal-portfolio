a5d5747a6e6918bac63fbd517c87fe2a2c57a09f
Donovan Tran <dontr710@gmail.com>
2025-03-26 09:56:05 -0700

    Make the gap smaller

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -20,27 +20,26 @@ export const Projects = () => {
       description: "Design & Development",
       imgUrl: projImg2,
     },
-    // Uncomment to test with more projects
-    // {
-    //   title: "Business Startup",
-    //   description: "Design & Development",
-    //   imgUrl: projImg3,
-    // },
-    // {
-    //   title: "Business Startup",
-    //   description: "Design & Development",
-    //   imgUrl: projImg1,
-    // },
-    // {
-    //   title: "Business Startup",
-    //   description: "Design & Development",
-    //   imgUrl: projImg2,
-    // },
-    // {
-    //   title: "Business Startup",
-    //   description: "Design & Development",
-    //   imgUrl: projImg3,
-    // },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
   ];
 
   // Dynamically calculate Col size based on the number of projects

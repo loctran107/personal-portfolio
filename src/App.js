4edf933c5a9b3a97582b51601e7dd88e2f6b1922
Donovan Tran <dontr710@gmail.com>
2025-03-30 01:53:44 -0700

    Add experience above

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
     <div className="App">
       <NavBar />
       <Banner />
-      <Skills />
       <ExperienceMap />
+      <Skills />
       <Projects />
       <Contact />
       <Footer />
